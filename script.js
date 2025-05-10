2b568e7041e32bb8d199d50b12fe06d6476318a2
Manmohan-coder <manmohanj8268@gmail.com>
2025-05-10 10:12:10 +0530

    back btn work

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const allElem = document.querySelectorAll('.elem')
 const allFullElem = document.querySelectorAll('.fullElem')
+const backBtn = document.querySelectorAll('.fullElem .back')
 
 allElem.forEach((elem)=>{
     elem.addEventListener('click',()=>{
         allFullElem[elem.id].style.display='block'
         
     })  
+})
+backBtn.forEach((backElem)=>{
+    backElem.addEventListener('click',()=>{
+        allFullElem[backElem.id].style.display = 'none'
+    })
+    
 })
